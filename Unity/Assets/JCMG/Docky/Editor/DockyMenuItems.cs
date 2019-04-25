989998418f4c267809b64463bb6a59256595beaa
Jeff Campbell <jeff@zapdot.com>
2019-04-25 09:12:13 +0200

    Removed optional COC feature until custom package dependencies can be made

--- Unity/Assets/JCMG/Docky/Editor/DockyMenuItems.cs
+++ Unity/Assets/JCMG/Docky/Editor/DockyMenuItems.cs
@@ -30,62 +30,6 @@ namespace JCMG.Docky.Editor
 {
 	public static class DockyMenuItems
     {
-#if JCMG_COC
-		[MenuItem("Tools/Docky/Refresh Readme Generation Options", priority = 0)]
-
-        public static void RefreshReadmeMenuItems()
-        {
-            // the generated filepath
-	        var filename = "DockyGeneratedMenuItems.cs";
-			var relativeScriptFilePath = string.Format("{0}/{1}", DockyInitializer.GetGeneratedFolder(), filename);
-	        var scriptFilePath = string.Format("{0}/{1}", Application.dataPath, relativeScriptFilePath.Remove(0, 7));
-
-			// The class string
-			var sb = new StringBuilder();
-            sb.AppendLine("// This class is Auto-Generated");
-            sb.AppendLine("using UnityEditor;");
-            sb.AppendLine("");
-            sb.AppendLine("public static class DockyGeneratedMenuItems");
-            sb.AppendLine("{");
-            sb.AppendLine("");
-
-            var counter = 50;
-            var dockyDefs = ReflectionUtility.GetAllDerivedInstancesOfType<DockyDefinition>().ToList();
-            dockyDefs.Sort(CompareDockyDefinitions);
-
-            foreach (var dockyDefinition in dockyDefs)
-            {
-                sb.AppendLine(string.Format("    [MenuItem(\"Tools/Docky/Create or Update {0}\", priority = {1})]", dockyDefinition.GetType().Name, counter++));
-                sb.AppendLine(string.Format("    private static void CreateOrUpdate{0}()", dockyDefinition.GetType().Name));
-                sb.AppendLine("    {");
-                sb.AppendLine(string.Format(@"
-        var dockyDef = new {0}();
-        dockyDef.Write();
-", dockyDefinition.GetType().FullName));
-
-
-                sb.AppendLine("    }");
-                sb.AppendLine("");
-            }
-
-            sb.AppendLine("");
-            sb.AppendLine("}");
-
-            // writes the class and imports it so it is visible in the Project window
-			if(File.Exists(scriptFilePath))
-				File.Delete(scriptFilePath);
-
-            File.WriteAllText(scriptFilePath, sb.ToString(), Encoding.UTF8);
-            AssetDatabase.ImportAsset(relativeScriptFilePath);
-
-        }
-
-        private static int CompareDockyDefinitions(DockyDefinition definition, DockyDefinition dockyDefinition1)
-        {
-            return definition.GetType().Name.CompareTo(dockyDefinition1.GetType().Name);
-        }
-#endif
-
 		[MenuItem("Tools/Docky/Create or Update All Readmes", priority = 10000)]
         public static void CreateOrUpdateReadme()
         {
